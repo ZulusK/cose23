04026f072911b31894aa5473703b2947c30098e9
IceBroForever <pasha1ruh@gmail.com>
2018-05-19 20:26:13 +0300

    Done flask

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
-from flask import Flask
-app = Flask(__name__)
+import json
+from flask import (
+    Flask,
+    render_template
+)
+from services.mongo_service import MongoService
+from models.message import Message
 
-@app.route('/')
-def hello():
-    return 'Hello world'
+app = Flask(__name__)
+db = MongoService("Posts")
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/posts/<author>', methods=['GET'])
+def posts(author):
+    posts = db.get('author', author)
+    ps = []
+    for post in posts:
+        p = {}
+        p['author'] = post.get('author')
+        p['date'] = post.get('date')
+        p['text'] = post.get('text')
+        p['topic_id'] = post.get('topic_id')
+        ps.append(p)
+    return json.dumps(ps)
