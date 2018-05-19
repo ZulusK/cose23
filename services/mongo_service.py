e9e86b0d31aff8309247c6efaff217a5690f716e
Artem Herasymov <artieherasymov@gmail.com>
2018-05-19 03:30:50 +0300

    Mongodb service added

---
+++ services/mongo_service.py
@@ -0,0 +1,26 @@
+from pymongo import MongoClient
+from src.message import Message
+
+
+class MongoService:
+
+    def __init__(self, collection):
+        Client = MongoClient()
+        db = Client["Cose2"]
+        self.collection = db[collection]
+
+    def insert(self, obj):
+        if isinstance(obj, Message):
+            print(obj.toJSON())
+            self.collection.insert(obj.__dict__)
+        else:
+            print("wrong type")
+
+    def get(self, key, value):
+        response = []
+        for rec in self.collection.find({key: value}):
+            response.append(rec)
+        return response
+
+    def remove(self, id):
+        self.collection.remove(id)
