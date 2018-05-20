eccd33eaf5f7cae678d2f83da00085010f90621f
Danil Kazimirov <danil.kazim.99@gmail.com>
2018-05-20 14:30:58 +0300

    fix merge

---
+++ forum_spider/settings.py
@@ -0,0 +1,15 @@
+BOT_NAME = 'forum_spider'
+
+SPIDER_MODULES = ['forum_spider.spiders']
+NEWSPIDER_MODULE = 'forum_spider.spiders'
+ROBOTSTXT_OBEY = True
+USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.170 Safari/537.36'
+CONCURRENT_REQUESTS = 32
+DOWNLOAD_DELAY = 1
+COOKIES_ENABLED = False
+ITEM_PIPELINES = {
+    'forum_spider.pipelines.MongoPipeline': 300,
+}
+MONGO_URI = 'mongodb://localhost:27017'
+MONGO_DATABASE = 'comments'
+LOG_ENABLED=False
---
+++ forum_spider/spiders/__init__.py
@@ -0,0 +1,1 @@
+from .flowersweb import FlowerswebSpider
