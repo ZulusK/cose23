05061f4a6dbf1d8a39976c608db2f90c7c78baf0
IceBroForever <pasha1ruh@gmail.com>
2018-06-02 14:41:07 +0300

    added pagination to requests

--- api/comment_resource.py
+++ api/comment_resource.py
@@ -1,9 +1,10 @@
+import math
 from flask_restful import Resource, reqparse
 from bson.objectid import ObjectId
 from db import CommentDB
+from .settings import ITEMS_PER_PAGE
 
 db = CommentDB()
-num_of_items_on_page = 10
 
 class CommentResource(Resource):
     def get(self):
@@ -13,22 +14,29 @@ class CommentResource(Resource):
         parser.add_argument('page', type=int, default=0)
         args = parser.parse_args()
 
-        first = args['page']*num_of_items_on_page
-        last = (args['page']+1)*num_of_items_on_page
         comments = None
         if args['user_id'] != None and args['topic_id'] != None:
             comments = db.find({
                 'user_id': ObjectId(args['user_id']),
                 'topic_id': ObjectId(args['topic_id'])
-            })[first:last]
+            })
         elif args['user_id'] != None:
-            comments = db.get_by_user_id(args['user_id'])[first:last]
+            comments = db.get_by_user_id(args['user_id'])
         elif args['topic_id'] != None:
-            comments = db.get_by_topic_id(args['topic_id'])[first:last]
+            comments = db.get_by_topic_id(args['topic_id'])
         else:
-            comments = db.get_all()[first:last]
+            comments = db.get_all()
+        
+        responce = {}
+        responce['page'] = args['page']
+        responce['items_per_page'] = ITEMS_PER_PAGE
+        responce['num_of_pages'] = math.ceil(len(comments) / ITEMS_PER_PAGE)
 
-        return comments
+        first = args['page'] * ITEMS_PER_PAGE
+        last = (args['page']+1) * ITEMS_PER_PAGE
+        responce['comments'] = comments[first:last]
+
+        return responce
 
 def config_comment_resources(api):
     api.add_resource(
--- api/user_resource.py
+++ api/user_resource.py
@@ -1,5 +1,7 @@
-from flask_restful import Resource, abort
+import math
+from flask_restful import Resource, abort, reqparse
 from db import UserDB
+from .settings import ITEMS_PER_PAGE
 
 db = UserDB()
 
@@ -13,10 +15,23 @@ class UserResource(Resource):
 
 class UserTopicsResource(Resource):
     def get(self, id):
+        parser = reqparse.RequestParser()
+        parser.add_argument('page', type=int, default=0)
+        args = parser.parse_args()
+
         user = db.get_by_id(id)
         if user == None:
             abort(404, message='No such user with id %s' % (id))
-        return user['topic_ids']
+        
+        responce = {}
+        responce['items_per_page'] = ITEMS_PER_PAGE
+        responce['page'] = args['page']
+        responce['num_of_pages'] = math.ceil(len(user['topic_ids'] / ITEMS_PER_PAGE))
+
+        first = args['page'] * ITEMS_PER_PAGE
+        last = (args['page'] + 1) * ITEMS_PER_PAGE
+        responce['topic_ids'] = user['topic_ids'][first:last]
+        return responce
 
 def config_user_resources(api):
     api.add_resource(
