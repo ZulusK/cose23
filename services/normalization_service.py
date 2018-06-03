86978176069dda33d85a94682f44f5aeca3b535a
Artem Herasymov <artieherasymov@gmail.com>
2018-06-03 11:58:13 +0300

    Data_processor for clusterization added

--- services/normalization_service.py
+++ services/normalization_service.py
@@ -4,19 +4,25 @@ import pymorphy2
 
 
 class NormalizationService:
+    def __init__(self, data):
+        self.data = data
 
-    def __init__(self, text):
-        self.text = text
+    def to_lowercase(self, text):
+        return text.lower()
 
-    def to_lowercase(self):
-        return self.text.lower()
+    def remove_artifacts(self, post):
+        post = re.sub(r'https?://[\S]+', ' url ', post)
+        post = re.sub(r'\d+ ?гг?', ' date ', post)
+        post = re.sub(r'\d+:\d+(:\d+)?', ' time ', post)
+        post = re.sub(r'@\w+', ' tname ', post)
+        post = re.sub(r'#\w+', ' htag ', post)
+        post = re.sub(r'[\W]+', ' ', post)
 
-    def remove_punctuation(self):
-        return re.sub(r'[^\w\s]', '', self.text)
+        return post
 
-    def to_basic_morph(self):
+    def to_basic_morph(self, post):
         basic_words = []
-        trimmed_text = self.remove_punctuation()
+        trimmed_text = self.remove_artifacts(post)
         word_list = trimmed_text.split()
         for word in word_list:
             morph = pymorphy2.MorphAnalyzer()
@@ -24,12 +30,13 @@ class NormalizationService:
             basic_words.append(normalized.normal_form)
         return ' '.join(basic_words)
 
-    def remove_stopwords(self):
-        return ' '.join([word for word in self.text.split() if word not in (stopwords.words('russian'))])
+    def remove_stopwords(self, post):
+        return ' '.join([word for word in post.split() if word not in (stopwords.words('russian'))])
 
     def normalize_text(self):
-        self.text = self.to_lowercase()
-        self.text = self.remove_stopwords()
-        self.text = self.to_basic_morph()
-        return self.text
-
+        for ind in range(self.data.__len__()):
+            if self.data[ind] is not None:
+                self.data[ind] = self.to_lowercase(self.data[ind])
+                self.data[ind] = self.remove_stopwords(self.data[ind])
+                self.data[ind] = self.to_basic_morph(self.data[ind])
+        return self.data
